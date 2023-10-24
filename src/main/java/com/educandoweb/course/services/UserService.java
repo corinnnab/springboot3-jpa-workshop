7c12d45231a153ade143004c73b35eb2b67a4f7b
Ana Corina <acorina59@gmail.com>
2023-10-24 11:46:44 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,11 +46,16 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		// Doesn't access database.
-		User entity = repository.getReferenceById(id);
-		updateData(entity, user);
+		try {
+			// Doesn't access database.
+			User entity = repository.getReferenceById(id);
+			updateData(entity, user);
+			
+			return repository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
-		return repository.save(entity);
 	}
 	
 	private void updateData(User entity, User user) {
